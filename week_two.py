5deb245036df8432b81c171c738eed362b54cf88
BlaQDeveloper <ikechukwudavid331@gmail.com>
2025-04-15 18:51:38 +0100

    Week 2 assignment done and dusted

--- week_two.py
+++ week_two.py
@@ -7,4 +7,6 @@ my_list.insert(1, 15) # This inserts 15 at the second position in my_list
 my_list.extend([50, 60, 70]) # This extends my_list with 50, 60, and 70
 my_list.pop()# This removes the last element from my_list
 my_list.sort() # This sorts my_list in ascending order.
-my_list.index(30) # This returns the index of 30 in my_list.
+index_of_30 = my_list.index(30) # This returns the index of 30 in my_list.
+print(index_of_30) # This prints the index of 30.
+print(my_list) # This prints the final state of my_list.
